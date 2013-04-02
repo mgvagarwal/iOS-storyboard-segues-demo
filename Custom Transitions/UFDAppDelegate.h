3f554b3c51a256ebc4c5db7e08271f7b72fb5861
Storyboard Presenter <storyboardpresenter@Scyther.local>
2013-04-03 00:18:27 +0200

    Initial Commit

---
+++ Custom Transitions/UFDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UFDAppDelegate.h
+//  Custom Transitions
+//
+//  Created by Storyboard Presenter on 4/3/13.
+//  Copyright (c) 2013 Ulrik Damm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UFDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Custom Transitions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Custom Transitions
+//
+//  Created by Storyboard Presenter on 4/3/13.
+//  Copyright (c) 2013 Ulrik Damm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UFDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([UFDAppDelegate class]));
+	}
+}
